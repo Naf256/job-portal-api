2cfce2d354e15d4d35051fdc2b55f9b76f49c84f
tokyaramen <nafis_256@yahoo.com>
2024-09-29 11:37:00 +0600

    changed schema file

--- schema.sql
+++ schema.sql
@@ -17,12 +17,15 @@ create table if not exists "companys" (
 );
 
 create table if not exists "jobs" (
-	"id"		   integer,
-	"title"		   text not null,
-	"experience"   text not null,
-	"location"	   text not null,
-	"description"  text not null,
-	"salary"       text not null,
+	"id"		    integer,
+	"title"		    text not null,
+	"experience"    text not null,
+	"location"	    text not null,
+	"description"   text not null,
+	"salary"        text not null,
+	"deadline"	    text,
+	"type"		    text,
+	"creation_date" text,
 	"company_id"   integer,
 	foreign key("company_id") references companys("id"),
 	primary key("id")
